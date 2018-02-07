0073fdc6d38197483ed5ee3b38e8a45beca77a98
NPP0804 <ajienaufal0804@gmail.com>
2018-02-08 03:06:34 +0700

    Tambahin file 2

--- bot.py
+++ bot.py
@@ -68,6 +68,34 @@ def fire_shot(opponent_map):
         shots = [tuple(map(int, shot.split(','))) for shot in f]
 
 
+    shotsDummy = shots
+    for shot in shotsDummy:
+        cell = searchCell(opponent_map, shot[0], shot[1])
+        if (cell['Missed']):
+            shots.remove(shot)
+        elif cell['Damaged']:
+            if (shot[1]+1 != map_size):
+                myCell = searchCell(opponent_map, shot[0], shot[1]+1)
+                if (not(myCell['Damaged']) and not(myCell['Missed'])):
+                    myShot = shot[0],shot[1]+1
+                    shots.insert(0,myShot)
+            if (shot[1] != 0):
+                myCell = searchCell(opponent_map, shot[0], shot[1]-1)
+                if (not(myCell['Damaged']) and not(myCell['Missed'])):
+                    myShot = shot[0],shot[1]-1
+                    shots.insert(0,myShot)
+            if (shot[0] != 0):
+                myCell = searchCell(opponent_map, shot[0]-1, shot[1])
+                if (not(myCell['Damaged']) and not(myCell['Missed'])):
+                    myShot = shot[0]-1,shot[1]
+                    shots.insert(0,myShot)
+            if (shot[0]+1 != map_size):
+                myCell = searchCell(opponent_map, shot[0]+1, shot[1])
+                if (not(myCell['Damaged']) and not(myCell['Missed'])):
+                    myShot = shot[0]+1,shot[1]
+                    shots.insert(0,myShot)
+            shots.remove(shot)
+
     i = 0
     check = False
     while (i<len(shots) and not(check)):
@@ -96,6 +124,11 @@ def fire_shot(opponent_map):
             f.write("\n")
     return
 
+
+def searchCell(opponent_map, x, y):
+    for cell in opponent_map:
+        if ((cell['X']==x) and (cell['Y']==y)):
+            return cell
 
 def place_ships():
     # Please place your ships in the following format <Shipname> <x> <y> <direction>
